387ab955c7ed11cfafd13e13bb68213590904ef8
Cole Fortner <colefortner@gmail.com>
2021-09-18 23:43:45 -0400

    Create sandwich tests using a before block

--- spec/sandwich_spec.rb
+++ spec/sandwich_spec.rb
@@ -3,6 +3,7 @@ require_relative '../sandwich.rb'
 describe Sandwich do
   context 'when the sandwich should be vegan' do
     let(:sandwich) { Sandwich.new(false, false, ['lettuce', 'tomato', 'mustard']) }
+
     it 'should not have meat' do
       # first we need to create a sandwich 
       # sandwich = Sandwich.new(false, false, ['lettuce', 'tomato', 'mustard'])
@@ -21,11 +22,25 @@ describe Sandwich do
     end
   end
 
-  # context 'when the sandwich should not be vegan' do
-  #   it 'should have meat'
+  context 'when the sandwich should not be vegan' do
+    before(:each) do
+      # variables defined here that are to be used later must be made an instance variable
+      @sandwich = Sandwich.new(true, true, ['lettuce', 'tomato', 'mayonaise'])
+    end
 
-  #   it 'should have cheese'
+    it 'should have meat' do
+      # sandwich = Sandwich.new(true, true, ['lettuce', 'tomato', 'mayonaise'])
+      expect(@sandwich.meat).to be(true)
+    end
 
-  #   it 'should have mayonaise'
-  # end
+    it 'should have cheese' do
+      # sandwich = Sandwich.new(true, true, ['lettuce', 'tomato', 'mayonaise'])
+      expect(@sandwich.cheese).to be(true)
+    end
+
+    it 'should have mayonaise' do
+      # sandwich = Sandwich.new(true, true, ['lettuce', 'tomato', 'mayonaise'])
+      expect(@sandwich.condiments).to include('mayonaise')
+    end
+  end
 end
