c12c83cde9b6cbf08c4f0b2d135e2abc82eafb5c
Cole Fortner <colefortner@gmail.com>
2021-09-18 23:35:21 -0400

    Introduce let to clean up sandwich tests

--- spec/sandwich_spec.rb
+++ spec/sandwich_spec.rb
@@ -2,20 +2,21 @@ require_relative '../sandwich.rb'
 
 describe Sandwich do
   context 'when the sandwich should be vegan' do
+    let(:sandwich) { Sandwich.new(false, false, ['lettuce', 'tomato', 'mustard']) }
     it 'should not have meat' do
       # first we need to create a sandwich 
-      sandwich = Sandwich.new(false, false, ['lettuce', 'tomato', 'mustard'])
+      # sandwich = Sandwich.new(false, false, ['lettuce', 'tomato', 'mustard'])
       # we created the sandwich and how we should expect certain things about the sandwich
       expect(sandwich.meat).to eq(false)
     end
 
     it 'should not have cheese' do
-      sandwich = Sandwich.new(false, false, ['lettuce', 'tomato', 'mustard'])
+      # sandwich = Sandwich.new(false, false, ['lettuce', 'tomato', 'mustard'])
       expect(sandwich.cheese).to eq(false)
     end
 
     it 'should not have mayonaise' do
-      sandwich = Sandwich.new(false, false, ['lettuce', 'tomato', 'mustard'])
+      # sandwich = Sandwich.new(false, false, ['lettuce', 'tomato', 'mustard'])
       expect(sandwich.condiments).to_not include('mayonaise')
     end
   end
